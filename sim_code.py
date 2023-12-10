56120e51d12da13aebbff92497777d1aff0d023b
MABL <tjs1980@rit.edu>
2023-12-10 00:57:24 -0500

    improved environment

--- sim_code.py
+++ sim_code.py
@@ -40,25 +40,12 @@ environment in coppeliaSim file
 
 import gym
 from gym import Env
-from gym.spaces import Discrete, Box, Dict, Tuple, MultiBinary, MultiDiscrete
+from gym.spaces import Discrete, Box
 
 import numpy as np
-import math
-import random
-import os
 
-#import stable_baselines3 
-#from stable_baselines3.common.vec_env 
-from random import randint
 from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 from time import sleep
-
-
-#usensors=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-#detect=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-#braitenbergL=[-0.2,-0.4,-0.6,-0.8,-1,-1.2,-1.4,-1.6, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-#braitenbergR=[-1.6,-1.4,-1.2,-1,-0.8,-0.6,-0.4,-0.2, 0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
-#v0=2
 
 '''
 We keep track of the direction of the robot so that we know to turn if we are not facing the direction of motion
@@ -79,7 +66,7 @@ class amigoEnv(gym.Env):
         self.minDetectionDist = 0.3
 
         self.destination_x = 1
-        self.destination_y = 6
+        self.destination_y = 1
         
         self.action_space = Discrete(4) 
         self.actions_max = 20
@@ -87,14 +74,6 @@ class amigoEnv(gym.Env):
         self.reward = 0
         self.done = False
         self.observations = np.zeros((22,1))
-        # change to np.array I guess.
-        self.position_x = 0
-        self.position_y = 0
-        self.orientation =  0
-        self.old_location_x = 0
-        self.old_location_y = 0
-        self.actions_taken =  0
-        print("loading scene")
 
         client = RemoteAPIClient('localhost', 23000)
         self.sim = client.getObject('sim')
@@ -108,12 +87,20 @@ class amigoEnv(gym.Env):
         # we might need to add shapes?
         self.sim.startSimulation()  
 
-        print("scene loaded. Running Proximity")
         self.initProximity()
 
+        pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
+        print(pos_of_sensor)
+
+        self.position_x = pos_of_sensor[0]
+        self.position_y = pos_of_sensor[1]
+        self.orientation =  0
+        self.old_location_x = pos_of_sensor[1]
+        self.old_location_y = pos_of_sensor[0]
+        self.actions_taken =  0
         # okay so we don't declare what the starting actions are here, we just set up the spaces for the stuff to be put into.
         
-        self.observation_space = Box(low = -255, high = 255, shape = (22,1), dtype = np.float32) # may need to specify type!
+        self.observation_space = Box(low = -255, high = 255, shape = (8,1), dtype = np.float32) # may need to specify type!
 
     def reset(self): # may need to be revised...
         print(f"reward = {self.reward}")
@@ -130,24 +117,26 @@ class amigoEnv(gym.Env):
         self.sim.startSimulation()  
 
         self.initProximity()
- 
-        self.position_x = 0
-        self.position_y = 0
+        pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
+        print(pos_of_sensor)
+
+        self.position_x = pos_of_sensor[0]
+        self.position_y = pos_of_sensor[1]
         self.orientation =  0
-        self.old_location_x = 0
-        self.old_location_y = 0
+        self.old_location_x = pos_of_sensor[1]
+        self.old_location_y = pos_of_sensor[0]
         self.actions_taken =  0
         self.reward = 0
         
-        self.observation_hold = [self.position_x, self.position_y, self.orientation, self.old_location_x, self.old_location_y, self.actions_taken] + list(self.detect)
+        self.observation_hold = [self.position_x, self.position_y, self.orientation, self.old_location_x, self.old_location_y, self.actions_taken, self.detect[3], self.detect[4]]
         # self.observations = np.zeros((22,1))
 
-        for i in range(0,22):
+        for i in range(0,8):
             self.observations[i] = self.observation_hold[i]
-        print("resetting")
         return self.observations, {}
     
     def step(self, action):
+        self.pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
         self.done = False
         done = self.done
 
@@ -174,85 +163,97 @@ class amigoEnv(gym.Env):
         # left right time
 
         if action == 0: # move forward
+            print('forward')
             if self.orientation != 90:
                 # put code to turn robot
                 self.move(-1, 1, 2.9)
                 orientation = 90
             # move forward
             self.move(1, 1, 10)
-            self.position_y += 1
+            self.pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
+            self.position_y = self.pos_of_sensor[1]
 
         elif action == 1: # move backwards
+            print('backwards')
             if self.orientation != 90:
                 # put code to turn robot
                 self.move(-1, 1, 2.9)
                 orientation = 90
             # move backwards
             self.move(-1, -1, 10)
-            self.position_y -= 1
+            self.pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
+            self.position_y = self.pos_of_sensor[1]
 
         elif action == 2: # move left
+            print('left')
             if self.orientation != 0:
                 # put code to turn robot
                 self.move(1, -1, 2.9)
                 orientation = 0
             # move backwards (left)
             self.move(-1, -1, 10)
-            self.position_x -= 1
+            self.pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
+            self.position_x = self.pos_of_sensor[0]
 
         elif action == 3: # move right
+            print('right')
             if self.orientation != 0:
                 # put code to turn robot
                 self.move(1, -1, 2.9)
                 orientation = 0
             # move foward (right)
             self.move(1, 1, 10)
-            self.position_x += 1
-
-        else: 
-            print("not an action!!")
+            self.pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
+            self.position_x = self.pos_of_sensor[0]
 
         # self.client.step() # I have no clue why this is here.
         ultrasonic_result = self.getProximity()
 
         if self.position_x == self.destination_x and self.position_y == self.destination_y: # end goal is to end up at this x,y location
-            self.reward += 50
+            self.reward += 500
             print("reached location")
             done = True
 
         # are we too close to an object?
-        for i in range(0,len(ultrasonic_result)):
-            if ultrasonic_result[i] < self.minDetectionDist-offset:
-                # avoiding objects is the most important task consequence of reward must be high
-                self.reward -= 500
-            else:
-                self.reward += 10
+ 
+        if ultrasonic_result[3] < self.minDetectionDist-offset:
+            # avoiding objects is the most important task consequence of reward must be high
+            self.reward -= 500
+            done = True
+        else:
+            self.reward += 10
 
         # did we advance towards the x coordinate of location?
         # it's okay for robot to move away from final dest to avoid object, so consequence will be lower
         if (self.destination_x-self.position_x) < (self.destination_x-self.old_location_x):
-            self.reward += 1
+            self.reward += 5
         else:
             self.reward -= 1
 
         # did we advance towards the y coordinate of location?
         if (self.destination_y-self.position_y) < (self.destination_y-self.old_location_y):
-            self.reward +=1
+            self.reward +=5
         else:
             self.reward -=1
 
-        if(self.position_x>=4) or self.position_x<=-5:
-            self.reward -= 500
-            done = True
-        if(self.position_y>=7) or self.position_y<=-2:
-            self.reward -= 500
-            done = True
+        # idk how to make sure that it doesnt fall off the edge
+        # if(self.position_x>=4) or self.position_x<=-5:
+        #     self.reward -= 500
+
+        # if(self.position_y>=7) or self.position_y<=-2:
+        #     self.reward -= 500
+
 
         self.observation_hold = [self.position_x, self.position_y, self.orientation, self.old_location_x, self.old_location_y, self.actions_taken] + list(self.detect)
         # self.observations = np.zeros((22,1))
 
         for i in range(0,22):
             self.observations[i] = self.observation_hold[i]
+
+        pos_of_sensor = self.sim.getObjectPosition(self.usensors[3])
+        print(pos_of_sensor)
+
+        print(self.observations)
 
         return (self.observations, self.reward, done, {}, {})
 
@@ -280,7 +281,7 @@ class amigoEnv(gym.Env):
                     # I am not sure that we need these calculations. May just add dist?
                 self.detect[i]=1-((dist-self.minDetectionDist)/(self.noDetectionDist-self.minDetectionDist))
             else:
-                self.detect[i]=0
+                self.detect[i]=6 # this is how we ensure it doesnt mistake no detection for 0!!
 
         return self.detect
     
--- training.py
+++ training.py
@@ -17,10 +17,6 @@ import gymnasium
 from gym import Env
 from gym.spaces import Discrete, Box, Dict, Tuple, MultiBinary, MultiDiscrete
 
-import numpy as np
-import math
-import random
-import os
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
@@ -35,12 +31,36 @@ from sim_code import amigoEnv #verify
 env = amigoEnv()
 # Wrap the environment with DummyVecEnv for compatibility
 # env = DummyVecEnv([lambda: env])
-log_path = "/home/mabl/tianna_ws/RL_Final_EE585/tensorboard/test1"
+save_path = "/home/mabl/tianna_ws/RL_Final_EE585/saved_models/test2"
+log_path = "/home/mabl/tianna_ws/RL_Final_EE585/tensorboard/test2"
 print("init model")
 model =  PPO('MlpPolicy', env, verbose = 1, tensorboard_log = log_path)
-model.learn(total_timesteps = 200)
-# Tianna, make a new dir to save your models in!!!
-# save_path = "/home/mabl/tianna_ws/RL_Final_EE585/saved_models/test1"
-# model.save(save_path)
-# evaluate_policy(model, env, n_eval_episodes = 10, render = True)
+
+for step in range(200):
+	# env.render()
+	# take random action
+	obs, reward, done, info, _ = env.step(env.action_space.sample())
+	print(reward, done)
+
 env.close()
+
+
+# n_episodes = 200
+# for episode in range(n_episodes):
+#     observation, _ = env.reset()
+#     action = 0
+#     next_observation, reward, done, _, _ = env.step(action)
+#     model.learn(total_timesteps=200, reset_num_timesteps=False)
+#     while done == False:
+#         action, _= model.predict(observation)
+#         next_observation, reward, done, _, _ = env.step(action)
+#         model.learn(total_timesteps=200, reset_num_timesteps=False)
+#         print("next obs")
+#         evaluate_policy(model, env, n_eval_episodes = 10, render = False)
+#         model.save(save_path)
+        
+        # observation = next_observation
+
+# model.learn(total_timesteps = 200)
+# Tianna, make a new dir to save your models in!!!
+# model.save(save_path)
